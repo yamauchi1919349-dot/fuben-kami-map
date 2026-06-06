172b2a6760b9cebad5b48072b218903f3492d859
Ryosuke <yamauchi1919349@gmail.com>
2026-06-06 22:25:51 +0900

    Improve iPhone app icon support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,17 +12,22 @@ export const metadata: Metadata = {
         url: "/icon.png",
         sizes: "1254x1254",
         type: "image/png"
+      },
+      {
+        url: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        url: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png"
       }
     ],
     apple: [
       {
         url: "/apple-touch-icon.png",
         sizes: "180x180",
-        type: "image/png"
-      },
-      {
-        url: "/icon.png",
-        sizes: "1254x1254",
         type: "image/png"
       }
     ],
